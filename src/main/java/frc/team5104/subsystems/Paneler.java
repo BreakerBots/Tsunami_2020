11430122cc9b7bed7df61bc24d0b78e5bdef0d8c
mchow101 <mintychocolate101@gmail.com>
2020-01-20 14:11:12 -0800

    control panel

--- src/main/java/frc/team5104/subsystems/Paneler.java
+++ src/main/java/frc/team5104/subsystems/Paneler.java
@@ -5,15 +5,36 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import frc.team5104.util.ColorSensor;
+import frc.team5104.util.ColorSensor.CoolColor;
+import frc.team5104.util.console;
 import frc.team5104.util.managers.Subsystem;
 
 public class Paneler extends Subsystem {
+	private static ColorSensor sensor;
 	private static TalonSRX talon;
+	private static Joystick joystick;
 	private static DoubleSolenoid piston;
+	private static CoolColor color;
 	
 	//Loop
 	public void update() {
-		
+		//console.log(readColor());
+		//console.log(atTargetPosition());
+		//double in = joystick.getRawAxis(1);
+		//setPercentOutput(in * 0.3);
+		/*if(atTargetPosition() == true) {
+			setPercentOutput(0);
+		} else {
+			setPercentOutput(0.3);*/
+		if(rotationControl() == true) {
+			setPercentOutput(0);
+		} else {
+			setPercentOutput(1);
+		}
+			
 	}
 
 	//Internal Functions
@@ -29,15 +50,38 @@ public class Paneler extends Subsystem {
 	
 	//External Functions
 	public static boolean atTargetPosition() {
-		return false;
+	String FMS = DriverStation.getInstance().getGameSpecificMessage();
+		if(FMS.charAt(0) == 'R' && sensor.getColor() == CoolColor.BLUE) {
+			//return true;
+		} else if(FMS.charAt(0) == 'Y' && sensor.getColor() == CoolColor.GREEN) {
+			return true;
+		} else if(FMS.charAt(0) == 'B' && sensor.getColor() == CoolColor.RED) {
+			return true;
+		} else if(FMS.charAt(0) == 'G' && sensor.getColor() == CoolColor.YELLOW) {
+			return true;
+		} 
+			return false;		
+	}	
+	public static CoolColor readColor() {
+		return sensor.getColor();
 	}
-	public static void readColor() {
-		
+	public static boolean rotationControl() {
+		double wheelRotations = talon.getSelectedSensorPosition() / (49 * 4096);
+		console.log(wheelRotations);
+		double cpRotations = (wheelRotations * 1.5) / 16;
+		if (cpRotations >= 3 && cpRotations <= 5) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	//Config
 	public void init() {
-		
+		sensor = new ColorSensor();
+		talon = new TalonSRX(21);
+		joystick = new Joystick(0);
+		talon.setSelectedSensorPosition(0);
 	}
 
 	//Reset
